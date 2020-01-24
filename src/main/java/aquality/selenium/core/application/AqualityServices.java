725cfec6d93847d0a1ff71b1685324b759410532
Sergey Knysh <se.knysh@gmail.com>
2020-01-24 16:21:02 +0300

    added unload to AqualitySerices

--- src/main/java/aquality/selenium/core/application/AqualityServices.java
+++ src/main/java/aquality/selenium/core/application/AqualityServices.java
@@ -7,42 +7,38 @@ import com.google.inject.Injector;
  * Describes static methods which help work with Application and dependency injector.
  * Injector is thread safe.
  */
-public abstract class AqualityServices {
+public class AqualityServices {
 
     private static final ThreadLocal<Injector> injectorContainer = new ThreadLocal<>();
 
     protected AqualityServices() {
     }
 
-
-    protected static Injector getInjector() {
+    /**
+     * Gets existing injector or initializes new with based on {@link AqualityModule}.
+     *
+     * @return existing or new injector.
+     */
+    public static Injector getInjector() {
         if (injectorContainer.get() == null) {
-            injectorContainer.remove();
             initInjector();
         }
 
         return injectorContainer.get();
     }
 
-    /**
-     * Sets default injector with {@link AqualityModule}.
-     */
     private static void initInjector() {
         initInjector(new AqualityModule());
     }
 
     /**
-     * Sets custom injector.
+     * Initializes custom injector.
      *
      * @param module - custom module with dependencies.
-     * @param <T>    is type of custom module.Custom module should be inherited from {@link AqualityModule}.
+     * @param <T> is type of custom module. Custom module should be inherited from {@link AqualityModule}.
      */
     protected static <T extends AqualityModule> void initInjector(T module) {
         injectorContainer.remove();
         injectorContainer.set(Guice.createInjector(module));
-    }
-
-    public void unload() {
-        injectorContainer.remove();
     }
 }
