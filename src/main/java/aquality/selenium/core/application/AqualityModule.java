80a501afb73effd6167d8e80318a2995f409d04e
Sergey Knysh <se.knysh@gmail.com>
2020-01-24 15:19:34 +0300

    added documentation

--- src/main/java/aquality/selenium/core/application/AqualityModule.java
+++ src/main/java/aquality/selenium/core/application/AqualityModule.java
@@ -3,8 +3,14 @@ package aquality.selenium.core.application;
 import com.google.inject.AbstractModule;
 import aquality.selenium.core.logging.Logger;
 
+/**
+ * Describes all dependencies which is registered for the project.
+ */
 public class AqualityModule extends AbstractModule {
 
+    /**
+     * Registers dependencies to a container.
+     */
     @Override
     protected void configure() {
         bind(Logger.class).toInstance(Logger.getInstance());
--- src/main/java/aquality/selenium/core/application/AqualityServices.java
+++ src/main/java/aquality/selenium/core/application/AqualityServices.java
@@ -3,6 +3,10 @@ package aquality.selenium.core.application;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
+/**
+ * Describes static methods which help work with Application and dependency injector.
+ * Injector is thread safe.
+ */
 public abstract class AqualityServices implements AutoCloseable {
 
     private static final ThreadLocal<Injector> injectorContainer = new ThreadLocal<>();
@@ -10,10 +14,22 @@ public abstract class AqualityServices implements AutoCloseable {
     protected AqualityServices() {
     }
 
+    /**
+     * Gets default injector with {@link AqualityModule}.
+     *
+     * @return injector.
+     */
     protected static Injector getInjector() {
         return getInjector(new AqualityModule());
     }
 
+    /**
+     * Gets custom injector.
+     *
+     * @param module is custom module with dependencies.
+     * @param <T>    type of custom module. Should be inherited from {@link AqualityModule}.
+     * @return custom injector.
+     */
     protected static <T extends AqualityModule> Injector getInjector(T module) {
         if (injectorContainer.get() == null) {
             setInjector(module);
@@ -22,15 +38,27 @@ public abstract class AqualityServices implements AutoCloseable {
         return injectorContainer.get();
     }
 
+    /**
+     * Sets default injector with {@link AqualityModule}.
+     */
     protected static void setInjector() {
         setInjector(new AqualityModule());
     }
 
+    /**
+     * Sets custom injector.
+     *
+     * @param module - custom module with dependencies.
+     * @param <T>    is type of custom module.Custom module should be inherited from {@link AqualityModule}.
+     */
     protected static <T extends AqualityModule> void setInjector(T module) {
         injectorContainer.remove();
         injectorContainer.set(Guice.createInjector(module));
     }
 
+    /**
+     * Closes thread safe container.
+     */
     @Override
     public void close() {
         injectorContainer.remove();
