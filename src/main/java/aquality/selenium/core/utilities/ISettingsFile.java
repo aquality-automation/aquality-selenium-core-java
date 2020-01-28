1c72731c2379af305d33ee5838b6093dcb66c61f
Sergey Knysh <se.knysh@gmail.com>
2020-01-28 10:14:42 +0300

    added documentation for ISettingsFile

--- src/main/java/aquality/selenium/core/utilities/ISettingsFile.java
+++ src/main/java/aquality/selenium/core/utilities/ISettingsFile.java
@@ -3,15 +3,47 @@ package aquality.selenium.core.utilities;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Describes reader of settings file.
+ */
 public interface ISettingsFile {
 
-    Object getValue(String jsonPath);
+    /**
+     * Gets value from settings file.
+     *
+     * @param path Path to the value. Depending on file format, it can be jsonPath, xpath etc.
+     * @return Value.
+     */
+    Object getValue(String path);
 
-    List<String> getList(String jsonPath);
+    /**
+     * Gets list of values from settings file.
+     *
+     * @param path Path to the value. Depending on file format, it can be jsonPath, xpath etc.
+     * @return List of values.
+     */
+    List<String> getList(String path);
 
-    Map<String, Object> getMap(String jsonPath);
+    /**
+     * Gets map of keys and values from settings file.
+     *
+     * @param path Path to the value. Depending on file format, it can be jsonPath, xpath etc.
+     * @return Map of keys and values.
+     */
+    Map<String, Object> getMap(String path);
 
+    /**
+     * Gets content of settings file.
+     *
+     * @return Content of settings file as string.
+     */
     String getContent();
 
+    /**
+     * Checks if value exists in settings.
+     *
+     * @param path Path to the values. Depending on file format, it can be jsonPath, xpath etc.
+     * @return True if exists, false otherwise.
+     */
     boolean isValuePresent(String path);
 }
