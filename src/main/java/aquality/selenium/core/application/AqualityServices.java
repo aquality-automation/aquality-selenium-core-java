3fa78729d2c149da8c86d3023a53d2ab6a31b592
Meleshko, Aleksey2 <a2.meleshko@a1qa.com>
2020-01-28 19:23:27 +0300

    Update AqualityServices documentation

--- src/main/java/aquality/selenium/core/application/AqualityServices.java
+++ src/main/java/aquality/selenium/core/application/AqualityServices.java
@@ -9,7 +9,6 @@ import java.util.function.Supplier;
 
 /**
  * Describes static methods which help work with Application and dependency injector.
- * Injector is thread safe.
  */
 public abstract class AqualityServices <T extends IApplication> {
 
@@ -24,14 +23,26 @@ public abstract class AqualityServices <T extends IApplication> {
         injector = Guice.createInjector(module);
     }
 
+    /**
+     * @return true if the application is already started, false otherwise.
+     */
     protected boolean isAppStarted() {
         return app != null && app.isStarted();
     }
 
+    /**
+     * Sets the application instance, saving it to DI container.
+     * @param application instance to set into container.
+     */
     protected void setApp(T application) {
         this.app = application;
     }
 
+    /**
+     * Returns an existing application or initializes a new one based on passed parameter.
+     * @param startApplicationFunction function to start the application, where the injector could be used.
+     * @return started application.
+     */
     protected T getApp(Function<Injector, T> startApplicationFunction) {
         if (!isAppStarted()) {
             setApp(startApplicationFunction.apply(injector));
@@ -40,6 +51,11 @@ public abstract class AqualityServices <T extends IApplication> {
         return app;
     }
 
+    /**
+     * Gets existing injector based on {@link AqualityModule} supplier passed to constructor.
+     *
+     * @return existing injector.
+     */
     protected Injector getInjector() {
         return injector;
     }
