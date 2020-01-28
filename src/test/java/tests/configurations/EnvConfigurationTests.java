5ad251975c2d86040ec12247aee52ee85aa4e31b
Sergey Knysh <se.knysh@gmail.com>
2020-01-28 18:44:58 +0300

    fixed naming and added empty line at the end of files

--- src/test/java/tests/configurations/EnvConfigurationTests.java
+++ src/test/java/tests/configurations/EnvConfigurationTests.java
@@ -17,18 +17,15 @@ import static org.testng.Assert.assertEquals;
 public class EnvConfigurationTests {
 
     private static final String LANGUAGE_KEY = "logger.language";
-    private static final String languageKey = "logger.language";
-    private static final String languageValue = "ru";
-    private static final String conditionTimeoutKey = "timeouts.timeoutCondition";
-    private static final String newTimeoutValue = "10000";
+    private static final String LANGUAGE_VALUE = "ru";
+    private static final String CONDITION_TIMEOUT_KEY = "timeouts.timeoutCondition";
+    private static final String NEW_TIMEOUT_VALUE = "10000";
     private Injector injector;
 
     @BeforeMethod
     public void before(){
         System.setProperty(LANGUAGE_KEY, "ru");
-    public void before() {
-        System.setProperty(languageKey, languageValue);
-        System.setProperty(conditionTimeoutKey, newTimeoutValue);
+        System.setProperty(CONDITION_TIMEOUT_KEY, NEW_TIMEOUT_VALUE);
         CustomAqualityServices.initInjector(new TestModule());
         injector = CustomAqualityServices.getInjector();
     }
@@ -42,12 +39,12 @@ public class EnvConfigurationTests {
     @Test
     public void testShouldBePossibleToOverrideTimeoutWithEnvVariable() {
         long conditionTimeout = injector.getInstance(ITimeoutConfiguration.class).getCondition();
-        assertEquals(conditionTimeout, Long.parseLong(newTimeoutValue), "Condition timeout should be overridden with env variable");
+        assertEquals(conditionTimeout, Long.parseLong(NEW_TIMEOUT_VALUE), "Condition timeout should be overridden with env variable");
     }
 
     @Test
     public void testNumberFormatExceptionShouldBeThrownIfTimeoutIsNotANumber() {
-        System.setProperty(conditionTimeoutKey, languageValue);
+        System.setProperty(CONDITION_TIMEOUT_KEY, LANGUAGE_VALUE);
         try {
             CustomAqualityServices.initInjector(new TestModule());
             CustomAqualityServices.getInjector().getInstance(ITimeoutConfiguration.class).getCommand();
@@ -60,10 +57,7 @@ public class EnvConfigurationTests {
     @AfterMethod
     public void after(){
         System.clearProperty(LANGUAGE_KEY);
+        System.clearProperty(CONDITION_TIMEOUT_KEY);
         CustomAqualityServices.initInjector(new TestModule());
-    public void after() {
-        System.clearProperty(languageKey);
-        System.clearProperty(conditionTimeoutKey);
-        CustomAqualityServices.initInjector(new AqualityModule());
     }
 }
