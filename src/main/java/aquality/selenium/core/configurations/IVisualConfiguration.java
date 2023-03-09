2274f821962feb4dea66531afb726ae131c07cc0
Alaksiej Mialeška <mialeszkaalaksiej@gmail.com>
2023-03-09 23:11:38 +0100

    Implement VisualConfiguration

--- src/main/java/aquality/selenium/core/configurations/IVisualConfiguration.java
+++ src/main/java/aquality/selenium/core/configurations/IVisualConfiguration.java
@@ -9,4 +9,34 @@ public interface IVisualConfiguration {
      * @return image format.
      */
     String getImageFormat();
+
+    /**
+     * Gets maximum length of full file name with path for image comparison.
+     * @return maximum symbols count in file path.
+     */
+    int getMaxFullFileNameLength();
+
+    /**
+     * Gets default threshold used for image comparison.
+     * @return The default threshold value.
+     */
+    float getDefaultThreshold();
+
+    /**
+     * Gets width of the image resized for comparison.
+     * @return comparison width.
+     */
+    int getComparisonWidth();
+
+    /**
+     * Gets height of the image resized for comparison.
+     * @return comparison height.
+     */
+    int getComparisonHeight();
+
+    /**
+     * Gets path used to save and load page dumps.
+     * @return path to dumps.
+     */
+    String getPathToDumps();
 }
--- src/main/java/aquality/selenium/core/configurations/VisualConfiguration.java
+++ src/main/java/aquality/selenium/core/configurations/VisualConfiguration.java
@@ -1,9 +1,12 @@
 package aquality.selenium.core.configurations;
 
+import aquality.selenium.core.logging.Logger;
 import aquality.selenium.core.utilities.ISettingsFile;
 import com.google.inject.Inject;
 
 import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 
 /**
@@ -12,6 +15,12 @@ import java.util.Arrays;
  */
 public class VisualConfiguration implements IVisualConfiguration {
     private String imageFormat;
+    private Integer maxFullFileNameLength;
+    private Float defaultThreshold;
+    private Integer comparisonWidth;
+    private Integer comparisonHeight;
+    private String pathToDumps;
+
     private final ISettingsFile settingsFile;
 
     /**
@@ -36,5 +45,58 @@ public class VisualConfiguration implements IVisualConfiguration {
             imageFormat = actualFormat;
         }
         return imageFormat;
+    }
+
+    @Override
+    public int getMaxFullFileNameLength() {
+        if (maxFullFileNameLength == null) {
+            maxFullFileNameLength = Integer.valueOf(
+                    settingsFile.getValueOrDefault("/visualization/maxFullFileNameLength", 255).toString());
+        }
+        return maxFullFileNameLength;
+    }
+
+    @Override
+    public float getDefaultThreshold() {
+        if (defaultThreshold == null) {
+            defaultThreshold = Float.valueOf(
+                    settingsFile.getValueOrDefault("/visualization/defaultThreshold", 0.012f).toString());
+        }
+        return defaultThreshold;
+    }
+
+    @Override
+    public int getComparisonWidth() {
+        if (comparisonWidth == null) {
+            comparisonWidth = Integer.valueOf(
+                    settingsFile.getValueOrDefault("/visualization/comparisonWidth", 16).toString());
+        }
+        return comparisonWidth;
+    }
+
+    @Override
+    public int getComparisonHeight() {
+        if (comparisonHeight == null) {
+            comparisonHeight = Integer.valueOf(
+                    settingsFile.getValueOrDefault("/visualization/comparisonHeight", 16).toString());
+        }
+        return comparisonHeight;
+    }
+
+    @Override
+    public String getPathToDumps() {
+        if (pathToDumps == null) {
+            pathToDumps = settingsFile.getValueOrDefault(".visualization.pathToDumps", "./src/test/resources/VisualDumps/").toString();
+            if (pathToDumps.startsWith(".")) {
+                try {
+                    pathToDumps = new File(pathToDumps).getCanonicalPath();
+                } catch (IOException e) {
+                    String errorMessage = "Failed to resolve path to dumps: " + e.getMessage();
+                    Logger.getInstance().fatal(errorMessage, e);
+                    throw new IllegalArgumentException(errorMessage, e);
+                }
+            }
+        }
+        return pathToDumps;
     }
 }
